57de84886c0dd97ac1082ec74b373a169f9dfaaa
sejigner <sejigner@gmail.com>
2023-03-19 18:18:39 +0900

    3주차 과제 주석 추가

--- week3/week3/assignmenet_1.c
+++ week3/week3/assignmenet_1.c
@@ -1,28 +1,34 @@
-#include <stdio.h>
+#include <stdio.h> // 표준입출력 라이브러리 포함
 
 
 int main(void) {
+    // 변수 선언
     char name[20];
     char department[20];
     char birth[20];
     char id[20];
 
+    // 이름 입력 요청
     printf("이름 : ");
-    scanf_s("%s", name, (int)sizeof(name));
-    getchar();
+    scanf_s("%s", name, (int)sizeof(name)); // 변수 name에 입력값 할당
+    getchar(); // 버퍼 비우기
+
+    // 생년월일 입력 요청
     printf("생년월일 : ");
-    scanf_s("%s", birth, (int)sizeof(birth));
-    getchar();
+    scanf_s("%s", birth, (int)sizeof(birth)); // 변수 birth에 입력값 할당
+    getchar(); // 버퍼 비우기
 
+    // 학번 입력 요청
     printf("학번 : ");
-    scanf_s("%s", id, (int)sizeof(id));
-    getchar();
+    scanf_s("%s", id, (int)sizeof(id)); // 변수 id에 입력값 할당
+    getchar(); // 버퍼 비우기
 
+    // 학과명 입력 요청
     printf("학과명 : ");
-    scanf_s("%s", department, (int)sizeof(department));
-    getchar();
+    scanf_s("%s", department, (int)sizeof(department)); // 변수 department에 입력값 할당
+    getchar(); // 버퍼 비우기
 
-
+    // 변수 출력
     printf("\n");
     printf("이름: %s\n", name);
     printf("생년월일: %s\n", birth);
